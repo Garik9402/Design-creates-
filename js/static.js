c4b0b01ce575f3ab7532458c1cb5d6c3d43c7733
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-06-28 18:10:37 +0300

    slaider in the process;)

--- js/static.js
+++ js/static.js
@@ -1,4 +1,15 @@
- let burger = document.querySelector('.burger')
- burger.addEventListener('click', function() {
-     burger.classList.toggle('burger--active-toggle')
- })
+$(document).ready(function() {
+    $('.clients__slider').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        // speed: 1000,
+        // autoplay: true,
+        centerMode: true,
+    })
+});
+
+let burger = document.querySelector('.burger')
+burger.addEventListener('click', function() {
+    burger.classList.toggle('burger--active-toggle')
+})
