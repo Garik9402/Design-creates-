3fc144f75534f17bc48aa8c810920c9041710158
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-06-29 14:48:03 +0300

    section-clients and slider made up ;)

--- js/static.js
+++ js/static.js
@@ -1,14 +1,16 @@
+// слайдер jq
 $(document).ready(function() {
     $('.clients__slider').slick({
         arrows: true,
         dots: true,
         slidesToShow: 1,
-        // speed: 1000,
-        // autoplay: true,
+        speed: 1000,
+        autoplay: true,
         centerMode: true,
+        infinite: false,
     })
 });
-
+// бургер меню
 let burger = document.querySelector('.burger')
 burger.addEventListener('click', function() {
     burger.classList.toggle('burger--active-toggle')
